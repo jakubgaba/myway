019052d347f686ce82e36f7e3f4769b18001b91e
jakubgaba <j.gaba@email.cz>
2024-12-17 21:37:05 +0100

    Start

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div class="sideTitle">
+    <div>
+    <p class="sideTitleName">Jakub Gaba</p>
+    <p class="sideTitleInfo">Developer/Design</p>
+</div>
+</div>
+  </div>
+`
+
+
