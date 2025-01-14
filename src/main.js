b3c2bbdb78b2b88698ece97e45a96ea36a9bd791
jakubgaba <j.gaba@email.cz>
2025-01-14 20:27:50 +0100

    Adding threeJS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './style.css';
 import {App} from './App.js';
 import {addHoverEffect} from "./mainDisplay/basicEffects.js";
 import {Loader} from "./loader.js";
@@ -7,13 +6,7 @@ import {Loader} from "./loader.js";
 document.addEventListener('DOMContentLoaded', () => {
   const app = new App();
   const loaderShown = sessionStorage.getItem('loaderShown');
-
-  if (!loaderShown) {
-    const appLoader = new Loader(document.querySelector('#app') || document.body);
-    appLoader.start();
-    // sessionStorage.setItem('loaderShown', 'true');
-  } else {
     app.initialize();
     addHoverEffect();
   }
-});
+);
