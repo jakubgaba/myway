537098f57c2f26d151dc7406454989f8986f027b
Jakub Gaba <j.gaba@email.cz>
2025-01-20 08:40:10 +0100

    Text

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ export class App {
             <div class="rightCornerWrapper">
                 ${addLineToSVGCorner()}
                 ${displayRightCorner()}
+                //Text
             </div>
         `;
     }
